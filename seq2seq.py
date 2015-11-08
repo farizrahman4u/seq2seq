c4ca6d8412a1aa5e49aa4064742c9b625a9c45c4
Fariz Rahman <farizrahman4u@gmail.com>
2015-11-08 13:17:58 +0530

    Added remember_state argument

--- seq2seq.py
+++ seq2seq.py
@@ -8,7 +8,7 @@ from lstm_decoder import LSTMDecoder
 class Seq2seq(Sequential):
 	def __init__(self, output_dim, hidden_dim,output_length, init='glorot_uniform', inner_init='orthogonal', forget_bias_init='one', activation='tanh', inner_activation='hard_sigmoid',
                  weights=None, truncate_gradient=-1,
-                 input_dim=None, input_length=None, hidden_state=None, batch_size=None, depth=1
+                 input_dim=None, input_length=None, hidden_state=None, batch_size=None, depth=1, remember_state=False,
                  ):
 
 		if not type(depth) == list:
@@ -31,19 +31,19 @@ class Seq2seq(Sequential):
 							  init=init,inner_init=inner_init, activation=activation, 
 							  inner_activation=inner_activation,weights=weights[decoder_weight_index],
 							  truncate_gradient = truncate_gradient, 
-							  hidden_state=hidden_state[decoder_state_index], batch_size=batch_size)
+							  hidden_state=hidden_state[decoder_state_index], batch_size=batch_size, remember_state=remember_state)
 
 		encoder = LSTMEncoder(input_dim=input_dim, output_dim=hidden_dim,init=init,
 							  inner_init=inner_init, activation=activation, 
 							  inner_activation=inner_activation,weights=weights[encoder_weight_index],
 							  truncate_gradient = truncate_gradient, input_length=input_length,
-							  hidden_state=hidden_state[encoder_state_index], batch_size=batch_size, decoder=decoder)
+							  hidden_state=hidden_state[encoder_state_index], batch_size=batch_size, decoder=decoder, remember_state=remember_state)
 
 		left_deep = [LSTMEncoder(input_dim=input_dim, output_dim=input_dim,init=init,
 							  inner_init=inner_init, activation=activation, 
 							  inner_activation=inner_activation,weights=weights[i],
 							  truncate_gradient = truncate_gradient, input_length=input_length,
-							  hidden_state=hidden_state[i], batch_size=batch_size, return_sequences=True)
+							  hidden_state=hidden_state[i], batch_size=batch_size, return_sequences=True, remember_state=remember_state)
 					for i in range(depth[0]-1)]
 
 
@@ -51,7 +51,7 @@ class Seq2seq(Sequential):
 							  inner_init=inner_init, activation=activation, 
 							  inner_activation=inner_activation,weights=weights[decoder_weight_index + 1 + i],
 							  truncate_gradient = truncate_gradient, input_length=input_length,
-							  hidden_state=hidden_state[decoder_state_index + 1 + i], batch_size=batch_size, return_sequences=True)
+							  hidden_state=hidden_state[decoder_state_index + 1 + i], batch_size=batch_size, return_sequences=True, remember_state=remember_state)
 					for i in range(depth[1]-1)]
 
 		dense = Dense(input_dim=hidden_dim, output_dim=output_dim)
@@ -77,17 +77,22 @@ class Seq2seq(Sequential):
 				state.append(self.layers[i].get_hidden_state())
 		return state	
 
-def set_hidden_state(self, state):
-	for i in range(len(self.layers)):
-		if hasattr(self.layers[i], 'state'):
-			self.layers[i].set_hidden_state(state[i])
+	def set_hidden_state(self, state):
+		for i in range(len(self.layers)):
+			if hasattr(self.layers[i], 'state'):
+				self.layers[i].set_hidden_state(state[i])
 
-def get_weights(self):
-	return [l.get_weights() for l in self.layers]
+	def reset_hidden_state(self):
+		for i in range(len(self.layers)):
+			if hasattr(self.layers[i], 'state'):
+				self.layers[i].set_hidden_state(self.layers[i].get_hidden_state() * 0)
 
-def set_weights(self, weights):
-	if len(self.layers) != len(weights):
-		raise Exception("Exactly " + str(len(self.layers)) + " weight arrays required " + 
-			str(len(weights)) + " given")
-	for l,w in zip(l,weights):
-		l.set_weights(w)
+	def get_weights(self):
+		return [l.get_weights() for l in self.layers]
+
+	def set_weights(self, weights):
+		if len(self.layers) != len(weights):
+			raise Exception("Exactly " + str(len(self.layers)) + " weight arrays required " + 
+				str(len(weights)) + " given")
+		for l,w in zip(l,weights):
+			l.set_weights(w)
