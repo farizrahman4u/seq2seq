d67163628d08a060ecd0ae4939172b4625042438
Fariz Rahman <farizrahman4u@gmail.com>
2015-11-10 01:36:10 +0530

    Update seq2seq.py

--- seq2seq.py
+++ seq2seq.py
@@ -4,8 +4,9 @@ from keras.layers.core import Dense
 
 from lstm_encoder import LSTMEncoder
 from lstm_decoder import LSTMDecoder, LSTMDecoder2
+from stateful_container import StatefulContainer
 
-class Seq2seq(Sequential):
+class Seq2seq(StatefulContainer):
 	def __init__(self, output_dim, hidden_dim,output_length, init='glorot_uniform', inner_init='orthogonal', forget_bias_init='one', activation='tanh', inner_activation='hard_sigmoid',
                  weights=None, truncate_gradient=-1,
                  input_dim=None, input_length=None, hidden_state=None, batch_size=None, depth=1, remember_state=False,
@@ -19,46 +20,40 @@ class Seq2seq(Sequential):
 			weights = [None] * (n_lstms + 1)
 
 		if hidden_state is None:
-			hidden_state = [None] * n_lstms
+			hidden_state = [None] * (n_lstms + 1)
 
-		encoder_weight_index = depth[0] - 1
-		decoder_weight_index = depth[0] + 1
-
-		encoder_state_index = depth[0] - 1
-		decoder_state_index = depth[0]
+		encoder_index = depth[0] - 1
+		decoder_index = depth[0] + 1
 
 		decoder = LSTMDecoder(dim=output_dim, hidden_dim=hidden_dim, output_length=output_length,
 							  init=init,inner_init=inner_init, activation=activation, 
-							  inner_activation=inner_activation,weights=weights[decoder_weight_index],
+							  inner_activation=inner_activation,weights=weights[decoder_index],
 							  truncate_gradient = truncate_gradient, 
-							  hidden_state=hidden_state[decoder_state_index], batch_size=batch_size, remember_state=remember_state)
+							  hidden_state=hidden_state[decoder_index], batch_size=batch_size, remember_state=False)
 
 		encoder = LSTMEncoder(input_dim=input_dim, output_dim=hidden_dim,init=init,
 							  inner_init=inner_init, activation=activation, 
-							  inner_activation=inner_activation,weights=weights[encoder_weight_index],
+							  inner_activation=inner_activation,weights=weights[encoder_index],
 							  truncate_gradient = truncate_gradient, input_length=input_length,
-							  hidden_state=hidden_state[encoder_state_index], batch_size=batch_size, decoder=decoder, remember_state=remember_state)
+							  hidden_state=hidden_state[encoder_index], batch_size=batch_size, remember_state=context_sensitive)
 
 		left_deep = [LSTMEncoder(input_dim=input_dim, output_dim=input_dim,init=init,
 							  inner_init=inner_init, activation=activation, 
 							  inner_activation=inner_activation,weights=weights[i],
 							  truncate_gradient = truncate_gradient, input_length=input_length,
-							  hidden_state=hidden_state[i], batch_size=batch_size, return_sequences=True, remember_state=remember_state)
+							  hidden_state=hidden_state[i], batch_size=batch_size, return_sequences=True, remember_state=context_sensitive)
 					for i in range(depth[0]-1)]
 
 
 		right_deep = [LSTMEncoder(input_dim=output_dim, output_dim=output_dim,init=init,
 							  inner_init=inner_init, activation=activation, 
-							  inner_activation=inner_activation,weights=weights[decoder_weight_index + 1 + i],
+							  inner_activation=inner_activation,weights=weights[decoder_index + 1 + i],
 							  truncate_gradient = truncate_gradient, input_length=input_length,
-							  hidden_state=hidden_state[decoder_state_index + 1 + i], batch_size=batch_size, return_sequences=True, remember_state=remember_state)
+							  hidden_state=hidden_state[decoder_index + 1 + i], batch_size=batch_size, return_sequences=True, remember_state=context_sensitive)
 					for i in range(depth[1]-1)]
 
-		nl = len(left_deep)
-		nr = len(right_deep)
-
 		dense = Dense(input_dim=hidden_dim, output_dim=output_dim)
-
+		encoder.broadcast_state(decoder)
 		if weights[depth[0]] is not None:
 			dense.set_weights(weights[depth[0]])
 		super(Seq2seq, self).__init__()
@@ -74,30 +69,3 @@ class Seq2seq(Sequential):
 		self.decoder = decoder
 		self.left_deep = left_deep
 		self.right_deep = right_deep
-
-	def get_hidden_state(self):
-		state = []
-		for i in range(len(self.layers)):
-			if hasattr(self.layers[i], 'state'):
-				state.append(self.layers[i].get_hidden_state())
-		return state	
-
-	def set_hidden_state(self, state):
-		for i in range(len(self.layers)):
-			if hasattr(self.layers[i], 'state'):
-				self.layers[i].set_hidden_state(state[i])
-
-	def reset_hidden_state(self):
-		for i in range(len(self.layers)):
-			if hasattr(self.layers[i], 'state'):
-				self.layers[i].set_hidden_state(self.layers[i].get_hidden_state() * 0)
-
-	def get_weights(self):
-		return [l.get_weights() for l in self.layers]
-
-	def set_weights(self, weights):
-		if len(self.layers) != len(weights):
-			raise Exception("Exactly " + str(len(self.layers)) + " weight arrays required " + 
-				str(len(weights)) + " given")
-		for l,w in zip(l,weights):
-			l.set_weights(w)
