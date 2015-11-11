6d3d930e4f142e65d82024fdd740004d9363d8ab
Fariz Rahman <farizrahman4u@gmail.com>
2015-11-11 14:00:56 +0530

    Update deep_lstm.py

--- deep_lstm.py
+++ deep_lstm.py
@@ -20,30 +20,30 @@ class DeepLSTM(StatefulContainer):
     	if weights is None:
     		weights = [None]*nlayer
     	if hidden_state is None:
-    		hidden_state = [None]*depth
+    		hidden_state = [None]*nlayer
 
     	super(DeepLSTM, self).__init__()
-    	def get_lstm(idim, odim, rs, i):
+    	def get_lstm(idim, odim, rs):
     		return lstm(input_dim=idim, output_dim=odim, init=init,
     				inner_init=inner_init, forget_bias_init=forget_bias_init,
     				activation=activation, inner_activation=inner_activation,
-    				weights=weights[i], truncate_gradient=truncate_gradient,
-    				input_length=input_length, hidden_state=hidden_state[i],
+    				weights=weights[len(self.layers)], truncate_gradient=truncate_gradient,
+    				input_length=input_length, hidden_state=hidden_state[len(self.layers)],
     				batch_size=batch_size, return_sequences=rs,
     				remember_state=remember_state, **kwargs)
     	if depth == 1:
-    		self.add(get_lstm(input_dim, output_dim, return_sequences, 0))
+    		self.add(get_lstm(input_dim, output_dim, return_sequences))
     	else:
     		lstms = []
 
-    		layer = get_lstm(input_dim, hidden_dim, inner_return_sequences, 0)
+    		layer = get_lstm(input_dim, hidden_dim, inner_return_sequences)
     		lstms.append(layer)
     		self.add(layer)
     		if not inner_return_sequences:
     			self.add(RepeatVector(input_length))
 
     		for i in range(depth-2):
-    			layer = get_lstm(hidden_dim, hidden_dim, inner_return_sequences, i+1)
+    			layer = get_lstm(hidden_dim, hidden_dim, inner_return_sequences)
        			lstms.append(layer)
     			self.add(layer)
     			if not inner_return_sequences:
@@ -55,5 +55,3 @@ class DeepLSTM(StatefulContainer):
 
     		for i in range(len(lstms)-1):#connect hidden layers.
     			lstms[i].broadcast_state(lstms[i+1])
-
-
