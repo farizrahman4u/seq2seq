7342308886a60e5c99fc001fc10e0db0317b7660
Fariz Rahman <farizrahman4u@gmail.com>
2015-12-15 22:03:02 +0530

    Fix  save_weights

--- seq2seq/stateful_container.py
+++ seq2seq/stateful_container.py
@@ -1,32 +1,44 @@
 import keras
 from keras.models import Sequential
-
+import numpy as np
 
 class StatefulContainer(Sequential):
 	def __init__(self):
 		self.state = []#Mark this container as a stateful layer. Required for nested models. 
 		super(StatefulContainer, self).__init__()
 	def get_hidden_state(self):
-		return [l.get_hidden_state() if hasattr(l, 'state') else None for l in layers]	
+		state = []
+		for l in self.layers:
+			if hasattr(l, 'state'):
+				state.append += l.get_hidden_state()
+			else:
+				state += [np.ndarray(0)]
+		return state
 
 	def set_hidden_state(self, state):
-		if len(self.layers) != len(state):
-			raise Exception("State list length must be equal to number of layers in container")
-		for l, s in zip(self.layers, state):
+		for l in self.layers:
 			if hasattr(l, 'state'):
-				l.set_hidden_state(s)
+				nb_states = len(l.state)
+				l.set_hidden_state(state[:nb_states])
+				state = state[nb_states:]
+			else:
+				state = state[1:]
 
 	def reset_hidden_state(self):
 		for i in range(len(self.layers)):
 			if hasattr(self.layers[i], 'state'):
-				self.layers[i].set_hidden_state(self.layers[i].get_hidden_state() * 0)
+				self.layers[i].set_hidden_state(map(lambda x:x*0, self.layers[i].get_hidden_state()))
 
 	def get_weights(self):
-		return [l.get_weights() for l in self.layers]
+		weights = super(StatefulContainer, self).get_weights()
+		state = self.get_hidden_state()
+		return weights + state
 
 	def set_weights(self, weights):
-		if len(self.layers) != len(weights):
-			raise Exception("Exactly " + str(len(self.layers)) + " weight arrays required " + 
-				str(len(weights)) + " given")
-		for l,w in zip(l,weights):
-			l.set_weights(w)
+		for  l in self.layers:
+			nb_params = len(l.params)
+			l.set_weights(weights[:nb_params])
+			weights = weights[nb_params:]
+		self.set_hidden_state(weights)
+
+
