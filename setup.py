fd4240fdde0fdbb48d4797f5cde61a9796722270
Fariz Rahman <farizrahman4u@gmail.com>
2016-06-30 07:32:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 install_requires = [
     'Theano',
-    'Keras==0.3.2'
+    'Keras'
 ]
 
 setup(
       name='seq2seq',
-      version='0.0.2',
+      version='0.1.0',
       description='Sequence to Sequence Learning with Keras',
       author='Fariz Rahman',
       author_email='farizrahman4u@gmail.com',
